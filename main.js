4cbcd4f9a086df37dbfbf91b6686ccecb84e2081
Praveen-Kumar-KS <66106461+Praveen-Kumar-KS@users.noreply.github.com>
2021-08-18 14:01:40 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// Get your own free OWM API key at https://www.openweathermap.org/appid - please do not re-use this key!
+// You don't need an API key for this to work at the moment, but this will change eventually.
+apik = "3045dd712ffe6e702e3245525ac7fa38"
+//kelvin to celcious
+
+/*btn.addEventListener('click', function(){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+        .then(res => res.json())
+         //.then(data => console.log(data))
+        .then(data => {
+            var nameval = data['name']
+            var descrip = data['weather']['0']['description']
+            var tempature = data['main']['temp']
+            var wndspd = data['wind']['speed']
+
+            city.innerHTML=`City: ${nameval}`
+            temp.innerHTML = `Temperature: ${ convertion(tempature)} C`
+            description.innerHTML = `Conditions: ${descrip}`
+            wind.innerHTML = `Wind Speed: ${wndspd} km/h`
+
+        })
+        .catch(err => alert('You entered Wrong city name'))
+    })*/
+// Refer Sample Code
